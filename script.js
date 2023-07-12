1a82f4aa98c8da412c84aa57f876520f7ca3a751
MichailasJakutis <picolas@gmail.com>
2023-07-12 19:13:33 +0300

    added gold and potion

--- script.js
+++ script.js
@@ -16,9 +16,14 @@ const player = document.querySelector('.player-hp')
 const enemy = document.querySelector('.enemy-hp')
 const playerInfo = document.querySelector('.player-info')
 const monsterInfo = document.querySelector('.enemy-info')
-const monsterArray  = ['enemy.png', 'monster2.png', 'monster3.png', 'monster4.png', 'monster5.png']
+const enemys  = ['enemy.png', 'monster2.png', 'monster3.png', 'monster4.png', 'monster5.png']
+const potionBtn = document.querySelector('.potion')
+const goldElement = document.querySelector('.gold')
+let monsterIndex = 0
+let gold = 0
 
-console.log(monsterArray)
+
+
 btn.onclick = () => {attack()}
 
 
@@ -26,20 +31,51 @@ let playerHp = 100;
 let monsterHp = 100;
 
 function attack() {
-  let playerDamage = Math.random().toFixed(1) * 50;
-  let monsterDamage = Math.random().toFixed(1) * 8;
- 
 
-  monsterHp -= playerDamage;
-  playerHp -= monsterDamage;
+  let playerDamage = Math.random().toFixed(1) * 50
+  let monsterDamage = Math.random().toFixed(1) * 8
+  let hit = Math.random().toFixed(1) * 5
 
-  player.style.width = playerHp + "%";
-  enemy.style.width = monsterHp + "%";
+
+  gold += hit 
+  
+  
+
+
+
+  goldElement.textContent = `Gold: ${gold} `
+
+  monsterHp -= playerDamage
+  playerHp -= monsterDamage
+
+  player.style.width = playerHp + "%"
+  enemy.style.width = monsterHp + "%"
+
   playerInfo.textContent = `Player HP: ${playerHp.toFixed(1)}`
   monsterInfo.textContent = `Monster HP: ${monsterHp.toFixed(1)}`
+
 console.log('player hp: ' + playerHp + ' dmg: ' + playerDamage)
-console.log('monster hp: ' + monsterHp + ' dmg: ' +monsterDamage)
+console.log('monster hp: ' + monsterHp + ' dmg: ' + monsterDamage )
 
-   
+potionBtn.onclick = () => {
+  if ( gold >= 50 ){
+    gold -= 50
+    playerHp = 100
+    player.style.width = playerHp + "%"
 
+    
+  }
+ 
+  goldElement.textContent = `Gold: ${gold} `
+  
 }
+
+// if (monsterHp <= 0) {
+//   monsterIndex++
+ 
+//   monsterHp = 100;
+//  console.log(monsterIndex)
+// }
+}
+
+
